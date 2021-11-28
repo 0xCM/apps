d2c96c9c8698c389a6ca618c26328bec45e0a722
Chris Moore <0xcm00@gmail.com>
2021-11-28 17:10:18 -0600

    .

--- apps/.bits.test/.cmd/config.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set ProjId=bits.test
-set AppName=z0.bits.test
-call %~dp0..\..\.cmd\project-config.cmd
-
--- apps/.logix.test/.cmd/config.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set ProjId=logix.test
-set AppName=z0.logix.test
-call %~dp0..\..\.cmd\project-config.cmd
-
