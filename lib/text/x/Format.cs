dc7f9b4a3e064f996250757ae8dd7ae830dce669
Chris Moore <0xcm00@gmail.com>
2021-11-30 00:11:02 -0600

    .

--- lib/text/x/Format.cs
+++ lib/text/x/Format.cs
@@ -9,6 +9,30 @@ namespace Z0
     partial class XText
     {
         [TextUtility]
+        public static string Format(this sbyte i, byte n)
+            => string.Format(RP.digits(n), i);
+
+        [TextUtility]
+        public static string Format(this byte i, byte n)
+            => string.Format(RP.digits(n), i);
+
+        [TextUtility]
+        public static string Format(this short i, byte n)
+            => string.Format(RP.digits(n), i);
+
+        [TextUtility]
+        public static string Format(this ushort i, byte n)
+            => string.Format(RP.digits(n), i);
+
+        [TextUtility]
+        public static string Format(this int i, byte n)
+            => string.Format(RP.digits(n), i);
+
+        [TextUtility]
+        public static string Format(this uint i, byte n)
+            => string.Format(RP.digits(n), i);
+
+        [TextUtility]
         public static string Format(this ReadOnlySpan<char> src)
             => new string(src);
 
