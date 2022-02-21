d916b2dc1135a8c7d5bc51631a63e210a62ae78e
Chris Moore <0xcm00@gmail.com>
2022-02-20 20:39:15 -0600

    .

--- apps/asm.core/src/intel.intrinsics/models/Description.cs
+++ apps/asm.core/src/intel.intrinsics/models/Description.cs
@@ -4,11 +4,6 @@
 //-----------------------------------------------------------------------------
 namespace Z0.Asm
 {
-    using System;
-    using System.Runtime.CompilerServices;
-
-    using static Root;
-
     partial class IntelIntrinsics
     {
         public struct Description : ITextual
