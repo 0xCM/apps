bd02c35ea94af55a859f5b97dce019df2017c079
Chris Moore <0xcm00@gmail.com>
2022-05-29 04:27:28 -0500

    .

--- apps/.cmd/commit.cmd
+++ apps/.cmd/commit.cmd
@@ -1,8 +1,2 @@
 @echo off
-set RepoId=z0.apps
-call %ControlScripts%\commit-config.cmd
-
-git add -A -v >> %CommitLog% 2>nul
-git commit -v -am "." >> %CommitLog% 2>nul
-git push
-git archive -v -o %RepoArchive% HEAD 2> %ArchiveLog%
+call %ZDev%\.cmd\commit.cmd
