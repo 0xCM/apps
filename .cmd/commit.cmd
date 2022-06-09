9c16f10746b329446fdc41ac270c18a7c45e1323
Chris Moore <0xcm00@gmail.com>
2022-06-09 01:23:23 -0500

    .

--- .cmd/commit.cmd
+++ .cmd/commit.cmd
@@ -1,12 +1,11 @@
 @echo off
 call %~dp0config.cmd
 
-set ArchiveDst=%ZArchive%\bin\source\z0.zip
-set RepoLogs=%ZArchive%\repos
-set ArchiveLog=%RepoLogs%\z0-git-archive.log
-set GitLogPath=%RepoLogs%\z0-git.log
+set RepoArchives=K:\z0\archives\repos
+set CommitLogPath=%RepoArchives%\z0-commit.log
+set ArchiveTarget=%RepoArchives%\z0.archive.zip
 
-git add -A -v >> %GitLogPath%
-git commit -am "." -v >> %GitLogPath%
-git push -v >> %GitLogPath%
-git archive -v -o %ArchiveDst% HEAD 2> %ArchiveLog%
+git add -A -v >> %CommitLogPath%
+git commit -am "." -v >> %CommitLogPath%
+git push -v >> %CommitLogPath%
+git archive -v -o %ArchiveTarget% HEAD
