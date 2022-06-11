51e1f403e0f7f2086f08d528f12b959a07840b26
Chris Moore <0xcm00@gmail.com>
2022-06-11 11:59:56 -0500

    .

--- libs/memory/XTend.cs
+++ libs/memory/XTend.cs
@@ -13,5 +13,17 @@ namespace Z0
     {
         const NumericKind Closure = Root.UnsignedInts;
 
+        public static ByteSize TotalSize(this HexDataRow[] src)
+            => src.Select(x => x.Data.Count).Sum();
+
+        public static ByteSize TotalSize(this Index<HexDataRow> src)
+            => src.Storage.TotalSize();
+
+        public static BinaryCode Compact(this HexDataRow[] src)
+            => HexEmitter.compact(src);
+
+        public static BinaryCode Compact(this Index<HexDataRow> src)
+            => src.Storage.Compact();
+
     }
 }
