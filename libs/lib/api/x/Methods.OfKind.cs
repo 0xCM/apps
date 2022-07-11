9a7f729b658aea0ee57ed7468fb032560d25d423
0xCM00 <0xCM00@gmail.com>
2022-07-11 00:49:04 -0500

    .

--- libs/lib/api/x/Methods.OfKind.cs
+++ libs/lib/api/x/Methods.OfKind.cs
@@ -4,9 +4,6 @@
 //-----------------------------------------------------------------------------
 namespace Z0
 {
-    using System;
-    using System.Reflection;
-
     partial class XApi
     {
         /// <summary>
@@ -17,5 +14,39 @@ namespace Z0
         [Op]
         public static MethodInfo[] OfKind(this MethodInfo[] src, ApiClassKind kind)
             => from m in src where m.ApiClass() == kind select m;
+
+        /// <summary>
+        /// Selects methods from a stream that accept and/or return intrinsic vectors
+        /// </summary>
+        /// <param name="src">The methods to examine</param>
+        [Op]
+        public static MethodInfo[] OfKind(this MethodInfo[] src, Vec128Type vk, bool total = false)
+            => src.Where(m => m.IsKind(vk,total));
+
+        /// <summary>
+        /// Selects methods from a stream that accept and/or return intrinsic vectors
+        /// </summary>
+        /// <param name="src">The methods to examine</param>
+        [Op]
+        public static MethodInfo[] OfKind(this MethodInfo[] src, Vec256Type vk, bool total = false)
+            => src.Where(m => m.IsKind(vk,total));
+
+        /// <summary>
+        /// Selects methods from a stream that accept and/or return intrinsic vectors
+        /// </summary>
+        /// <param name="src">The methods to examine</param>
+        [Op, Closures(Closure)]
+        public static MethodInfo[] OfKind<T>(this MethodInfo[] src, Vec128Kind<T> vk)
+            where T : unmanaged
+                => src.Where(m => m.IsKind(vk));
+
+        /// <summary>
+        /// Selects methods from a stream that accept and/or return intrinsic vectors
+        /// </summary>
+        /// <param name="src">The methods to examine</param>
+        [Op, Closures(Closure)]
+        public static MethodInfo[] OfKind<T>(this MethodInfo[] src, Vec256Kind<T> vk)
+            where T : unmanaged
+                => src.Where(m => m.IsKind(vk));
     }
 }
