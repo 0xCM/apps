3485a564c10731aed9789be3387e04df3112f2ef
0xCM00 <0xCM00@gmail.com>
2022-07-28 10:02:29 -0500

    .

--- libs/wf/src/api/services/ApiPacks.cs
+++ libs/wf/src/api/services/ApiPacks.cs
@@ -4,11 +4,64 @@
 //-----------------------------------------------------------------------------
 namespace Z0
 {
+    using static Spans;
+    using static Arrays;
     using static ApiAtomic;
 
     [ApiHost]
     public sealed class ApiPacks : WfSvc<ApiPacks>
     {
+
+        public static ReadOnlySeq<IApiPack> discover()
+        {
+            var src = AppDb.Service.Capture().Root.SubDirs(false);
+            var dst = Lists.list<IApiPack>();
+            var counter = 0u;
+            for(var i=0; i<src.Count; i++)
+            {
+                ref readonly var source = ref src[i];
+                if(ApiPack.parse(source, out ApiPack pack))
+                {
+                    dst.Add(pack);
+                    counter++;
+                }
+            }
+
+            return slice(@readonly(dst.Seal()),0,counter).ToArray();
+        }
+
+        public static IApiPack create()
+            => new ApiPack(AppDb.Service.Capture().Targets(AppDb.Ts.Format()).Root, AppDb.Ts);
+
+        public static IApiPack create(Timestamp ts)
+            => new ApiPack(AppDb.Service.Capture().Targets(ts.Format()).Root, ts);
+
+        public static bool timestamp(FS.FolderPath src, out Timestamp dst)
+        {
+            dst = default;
+            var fmt = src.Format(PathSeparator.FS);
+            var idx = fmt.LastIndexOf(Chars.FSlash);
+            if(idx == NotFound)
+                return false;
+            return Time.parse(fmt.RightOfIndex(idx), out dst);
+        }
+
+        public static bool parse(FS.FolderPath src, out ApiPack dst)
+        {
+            dst = default;
+            var fmt = src.Format(PathSeparator.FS);
+            var idx = fmt.LastIndexOf(Chars.FSlash);
+            if(idx == NotFound)
+                return false;
+            var result =Time.parse(fmt.RightOfIndex(idx), out var ts);
+            if(result)
+                dst = new ApiPack(src,ts);
+            else
+                dst = new ApiPack(FS.FolderPath.Empty, Timestamp.Zero);
+            return result;
+        }
+
+
         public IApiPack Current()
             => ApiPack.discover().Last;
 
